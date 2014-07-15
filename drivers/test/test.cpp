77efd5f2e59eadd29a6df060434e1431b331d2b7
John Demme <jdd@cs.columbia.edu>
2014-07-15 18:19:30 -0400

    Testing my changes to get more info on prim_defs

--- drivers/test/test.cpp
+++ drivers/test/test.cpp
@@ -2,16 +2,27 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <set>
+
+#include <boost/foreach.hpp>
 
 using namespace std;
 
 #include <xdlrc/parser/XDLRCParser.hpp>
 #include <xdlrc/model/Device.hpp>
+#include <xdlrc/model/Element.hpp>
+
+using namespace bil;
 
 int main(int argc, const char** argv) {
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <xdlrc file>\n", argv[0]);
         return 1;
+    }
+
+    set<string> prims;
+    for (int i=2; i<argc; i++) {
+        prims.insert(argv[i]);
     }
 
     string xdlrcFileName = argv[1];
@@ -30,6 +41,30 @@ int main(int argc, const char** argv) {
     printf("Done.\n");
 
     printf("Read in device information for: %s\n", device.name().c_str());
+
+    printf("Primitive defs: \n");
+    auto types = device.primitiveTypes();
+    BOOST_FOREACH(auto type, types) {
+        printf("\t%s\n", type.name().c_str());
+        if (prims.count(type.name()) > 0) {
+            const Elements& es = type.elements();
+            BOOST_FOREACH(auto& e, es) {
+                printf("\t\t%s\n", e.name().c_str());
+                const auto& pins = e.pins();
+                BOOST_FOREACH(const auto& p, pins) {
+                    if (p.isInput())
+                        printf("\t\t\t%s input\n", p.externalName().c_str());
+                    else
+                        printf("\t\t\t%s output\n", p.externalName().c_str());
+                }
+                const auto& conns = e.conns();
+                BOOST_FOREACH(const auto& c, conns) {
+                    printf("\t\t\t%s %s.%s\n", c.pin().c_str(),
+                           c.dstElement().c_str(), c.dstPin().c_str());
+                }
+            }
+        }
+    }
 
     return 0;
 }
